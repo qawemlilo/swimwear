a4aa787d84249f27492f17c860c596f3b18a7062
Qawelesizwe <qawemlilo@gmail.com>
2012-09-27 13:38:38 +0200

    Project init

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8" lang="en"><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en"><!--<![endif]-->
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8"><meta charset="utf-8">
+  <title>Raging Flame Laboratory</title>
+  <meta name="description" content="Raging Flame Laboratory - web and software development">
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="Raging%20Flame%20Laboratory_files/bootstrap.css">
+  <link rel="stylesheet" href="Raging%20Flame%20Laboratory_files/bootstrap-responsive.css">
+  <link rel="stylesheet" href="Raging%20Flame%20Laboratory_files/style.css">
+  <link rel="stylesheet" href="Raging%20Flame%20Laboratory_files/media-queries.css">
+</head>
+<body>
+  <div class="navbar navbar-fixed-top">
+    <div class="navbar-inner">
+      <div class="container">
+        <a href="http://www.rflab.co.za/" class="brand pull-left"> Raging Flame Lab <i class="icon-fire icon-white"> </i></a>
+        <a data-toggle="collapse" data-target=".nav-collapse" class="btn btn-navbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"> </span>
+        </a>
+        <div class="nav-collapse collapse">
+          <ul id="nav-list" class="nav pull-right">
+            <li class="active">
+              <a href="http://www.rflab.co.za/">Home</a>
+            </li>
+            <li>
+              <a href="http://qawemlilo.rflab.co.za/" target="_blank">Blog</a>
+            </li>
+            <li>
+              <a href="http://www.rflab.co.za/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div id="home" class="container content container-fluid">
+  
+    <div class="row-fluid">
+      <div class="span6 visible-desktop"><img src="Raging%20Flame%20Laboratory_files/phones.png"></div>
+      <div class="span6">
+        <div id="app-name" class="visible-desktop"><h1>RF Lab</h1></div>
+        <div id="tagline">We create flexible, multi-device and future-proof websites </div>
+        <div id="phones" class="hidden-desktop"><img src="Raging%20Flame%20Laboratory_files/phones.png"></div>
+        <div id="description">
+          Raging Flame Lab is a Web design and development studio. We are mainly focused on Responsive Web Design, i.e, we build websites that are compatible with desktops, smart phones, and tablets. We offer the following services: 
+        </div>
+        <ul id="features">
+          <li>HTML5 Responsive websites</li>
+          <li>Content Management Solutions (Joomla! and WordPress)</li>
+          <li>Custom Joomla! extensions and upgrades   </li>
+          <li>Website Redesign</li>
+          <li>Website maintanance and management</li>
+          <li>Consultation and technical support </li>
+          <li>Web Hosting (including Node.js apps)</li>
+        </ul>
+        <div class="download-box">
+          <a href="http://www.rflab.co.za/contact" class="btn btn-primary btn-large">Get in touch</a>
+        </div>
+      </div>
+    </div>
+    
+    <div class="row-fluid">
+      <div class="span6">
+        <h3 id="scroll_up" class="page-title"> 
+          <a href="#home" class="arrow-top"><img src="Raging%20Flame%20Laboratory_files/arrow-top.png"></a>
+          What is a Responsive Website?
+        </h3>
+        <p>At its simplest, a responsive website adapts to the screen size of the visitor. It doesn't matter if the site visitor is using a Mac, a PC, a tiny mobile device, or a massive wide-screen; Windows, OSX, or Linux - responsive websites are device agnostic. </p>
+      </div>
+      
+      <div id="updates" class="span6 updates">
+        <h3 id="scroll_up" class="page-title"> 
+          <a href="#home" class="arrow-top"><img src="Raging%20Flame%20Laboratory_files/arrow-top.png"></a>
+          Why choose Responsive Web Design?
+        </h3>
+        <p>More and more people these days are turning to their mobile devices to search for business and product information, so your old site may not be working for you as well as it could.</p>
+        <p>Responsive design allows your site to work across a range of devices, from small to large without losing any of it's visual appeal or organizational integrity. Not only is this important for branding purposes, but it also ensures that your customers get the information they are looking for efficiently.
+        </p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="footer container container-fluid">
+    <div id="copyright"> 
+      <i class="icon-fire"> </i>
+      <a rel="license" href="http://www.rflab.co.za/">Raging Flame 2012</a>
+    </div>
+    
+    <div id="credits">  
+      <a href="http://qawemlilo.rflab.co.za/" target="_blank">Blog  </a>
+      <a href="http://www.rflab.co.za/contact">Contact</a>
+      <a href="http://twitter.com/ragingflameblog">Twitter</a>
+    </div>
+  </div>
+</body>
+</html>
